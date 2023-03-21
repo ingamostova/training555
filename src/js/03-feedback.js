267a438f04f57327ef44786d66a3c32f105ff509
Інга Мостова <ingo4ka20041997@gmail.com>
2023-03-21 18:20:29 +0200

    3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+const form = document.querySelector('form');
+const KEY = 'feedback-form-state';
 
+form.addEventListener('input', onInput);
+form.addEventListener('submit', onSubmit);
+onReset();
+
+function onInput(evt) {
+  const { email, message } = evt.currentTarget.elements;
+  localStorage.setItem(
+    KEY,
+    JSON.stringify({ email: email.value, message: message.value })
+  );
+}
+
+function onReset() {
+  const data = JSON.parse(localStorage.getItem(KEY));
+  //   console.log(data);
+  form.elements.email.value = data !== null ? data.email : '';
+  form.elements.message.value = data !== null ? data.message : '';
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const data = JSON.parse(localStorage.getItem(KEY)) || {};
+  console.log(data);
+  localStorage.clear();
+  evt.currentTarget.reset();
+}
